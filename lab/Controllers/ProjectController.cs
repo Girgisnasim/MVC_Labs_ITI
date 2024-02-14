c198c2577404c183c9c60b1e5cb82aeea8c1ac41
Girgis <gergesnaseem53@gmail.com>
2024-02-14 21:59:34 +0200

    add viwes for project update project controller update links in all files

--- lab/Controllers/ProjectController.cs
+++ lab/Controllers/ProjectController.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using lab.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace lab.Controllers
 {
     public class ProjectController : Controller
     {
+        private ITIContext context;
+        public ProjectController()
+        {
+            context = new ITIContext();
+        }
+
+        //get all Projects
         public IActionResult Index()
         {
-            return View();
+            List<Project> projects = context.Project.Include(p=>p.DeptProject).ToList();
+            return View(projects);
+        }   
+
+        //get project by id
+        public IActionResult Details(int id)
+        {
+            Project pro=context.Project.Include(p => p.DeptProject).SingleOrDefault(p=>p.Pnumber==id);
+            return View(pro);
         }
     }
 }
