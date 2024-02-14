3cd2510f93a11eb9389da654a827d1ccd3784d21
Girgis <gergesnaseem53@gmail.com>
2024-02-14 23:25:09 +0200

    start in CRUD Operations with add project

--- lab/Controllers/ProjectController.cs
+++ lab/Controllers/ProjectController.cs
@@ -25,5 +25,30 @@ namespace lab.Controllers
             Project pro=context.Project.Include(p => p.DeptProject).SingleOrDefault(p=>p.Pnumber==id);
             return View(pro);
         }
+
+        //CRUD Operations
+        //Add or create
+        //disply form
+        public IActionResult GetAddForm()
+        {
+            List<Department> departments=context.Department.ToList();
+            ViewData["departments"]=departments;
+            return View(departments);
+        }
+        //get form data
+        public IActionResult GetEditForm(string name ,string Ploc, string city,int Dnum)
+        {
+            Project project = new()
+            {
+                Pname = name,
+                Plocation = Ploc,
+                City = city,
+                Dnum = Dnum
+                
+            };
+            context.Project.Add(project);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
