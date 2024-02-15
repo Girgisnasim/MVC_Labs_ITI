dd1ded9262921489bff38c35aa67f955500f2c66
Girgis <gergesnaseem53@gmail.com>
2024-02-15 13:51:18 +0200

    add department controller

---
+++ lab/Controllers/DepartmentController.cs
@@ -0,0 +1,27 @@
+﻿using lab.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace lab.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private ITIContext context;
+        public DepartmentController()
+        {
+            context = new ITIContext();
+        }
+        //get all Department
+        public IActionResult Index()
+        {
+            List<Department> departments = context.Department.ToList();
+            return View(departments);
+        }
+        //get department by id
+        public IActionResult Details(int id)
+        {
+            Department department = context.Department.Include(d => d.Leader).SingleOrDefault(d => d.Dnum == id);
+            return View(department);
+        }
+    }
+}
